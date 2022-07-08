9e42f664e3d4ec36e664ade8710abbfa8cb36a78
nikolaklenje <nikolaelite88@gmail.com>
2022-07-08 13:47:32 -0400

    StaticPage

--- ReactApp/study/src/index.js
+++ ReactApp/study/src/index.js
@@ -1,18 +1,23 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-
 const page = (
   <div>
-    <h1> Welcome to React</h1>
-    <ul>
-      <li>I</li>
-      <li>Love</li>
-      <li>React</li>
-    </ul>
-    <h2>Welcome React</h2>
-    <p>Goodbye Restaurant</p>
+    <img src="/logo192.png" alt="" width="40px"></img>
+    <h1>Fun Facts About React</h1>
+  <ul>
+    <li>Was first released in 2013</li>
+    <li>Was originally created by Jordan Walke</li>
+    <li>Has over 100k stars on Github</li>
+    <li>It is maintained by Facebook</li>
+    <li>Powers thousands of enterprise aps, including mobile apps</li>
+  </ul>
+
   </div>
 )
-ReactDOM.render(page,
-  document.getElementById("root"))
+
+ReactDOM.render(
+  page,
+  document.getElementById("root")
+)
+//ReactDOM.render(page, document.getElementById("root") )
