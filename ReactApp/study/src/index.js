4b2e4f2edc6bed2d32978dbb14f563980cc54932
nikolaklenje <nikolaelite88@gmail.com>
2022-07-08 12:48:04 -0400

    Setup app

---
+++ ReactApp/study/src/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+const page = (
+  <div>
+    <h1> Welcome to React</h1>
+    <ul>
+      <li>I</li>
+      <li>Love</li>
+      <li>React</li>
+    </ul>
+    <h2>Welcome React</h2>
+    <p>Goodbye Restaurant</p>
+  </div>
+)
+ReactDOM.render(page,
+  document.getElementById("root"))
