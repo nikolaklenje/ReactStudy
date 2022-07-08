e1fcc2014c0be604cf9ea2584a99e5d7199a6437
nikolaklenje <nikolaelite88@gmail.com>
2022-07-07 20:14:02 -0400

    Second

--- Lesson1/index.js
+++ Lesson1/index.js
@@ -1,2 +1,4 @@
 ReactDOM.render(<h1>Hello,everyone!!!</h1>, document.getElementById("root"))
+//Pulling ReactDOM library,which gives us acces to render(give as ability to append a child 
+// to element)
 //first element is what I wanna put into file,and second element is where I want to put
---
+++ practice/index.js
@@ -0,0 +1,5 @@
+ReactDOM.render(<ul>
+    <li>I</li>
+    <li>LOVE</li>
+    <li>REACT</li>
+</ul>, document.getElementById("root"))
