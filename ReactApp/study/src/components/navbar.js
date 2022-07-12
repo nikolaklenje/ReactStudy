92f9b0fe87c3135b2bb0cf689aebb6c2cbe0bf82
nikolaklenje <nikolaelite88@gmail.com>
2022-07-12 19:12:32 -0400

    Add

---
+++ ReactApp/study/src/components/navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar(){return(
+    <nav>
+    <img className="logo" src="./logo192.png" alt=""></img><h1 className="nav-text-left" >ReactFacts</h1>
+    <h2 className ="nav-text-right">React Course - Project 1</h2>
+    </nav>
+
+)}
--- ReactApp/study/src/index.js
+++ ReactApp/study/src/index.js
@@ -1,19 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./style.css"
-import Header from "./Header"
-import Footer from "./Footer"
-import MainContent from "./MainContent"
+import App from "./App.js"
 
 
-function Page() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    )
-}
-
-ReactDOM.render(<Page />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
