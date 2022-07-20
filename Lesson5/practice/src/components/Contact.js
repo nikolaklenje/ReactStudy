a3597f6bf97aa6f8e684795f3a9e968368422949
nikolaklenje <nikolaelite88@gmail.com>
2022-07-20 07:55:44 -0400

    New

--- Lesson5/practice/src/components/Contact.js
+++ Lesson5/practice/src/components/Contact.js
@@ -16,3 +16,25 @@ return(
                 </div>
             </div>
 )}
+/*
+import React from "react"
+
+// Challenge: fix the bug, now that we've destructured the props object
+export default function Contact({img, name, phone, email}) {
+    return (
+        <div className="contact-card">
+            <img src={img}/>
+            <h3>{name}</h3>
+            <div className="info-group">
+                <img src="./images/phone-icon.png" />
+                <p>{phone}</p>
+            </div>
+            <div className="info-group">
+                <img src="./images/mail-icon.png" />
+                <p>{email}</p>
+            </div>
+        </div>
+    )
+}
+
+*/ 
---
+++ Lesson6/jokes/src/App.js
@@ -0,0 +1,29 @@
+import React from "react"
+import './App.css';
+import Joke from "./components/joke.js"
+
+export default function App(){return(
+<div>
+<Joke 
+Setup="I got my daughter a fridge for her birthday."
+Punchline= "I can't wait to see her face light up when she opens it."
+/>
+<Joke 
+Setup="How did the hacker escape the police?"
+Punchline= "Punchline: He just ransomware!"
+/>
+<Joke 
+Setup="Why don't pirates travel on mountain roads?"
+Punchline= " Scurvy."
+/>
+<Joke 
+Setup="What's the best thing about Switzerland?"
+Punchline= "I don't know, but the flag is a big plus!"
+/>
+<Joke 
+
+Punchline= "It is hard to explain puns to kleptomaniacs because they always take things literally."
+/>
+</div>
+
+)}
